e331d886c904b53f91cca32eb73ad211f5eb1982
rviu <134678217+rviu@users.noreply.github.com>
2023-05-26 19:37:48 +0000

    Create a simple sphere

--- main.cpp
+++ main.cpp
@@ -5,7 +5,23 @@
 #include "color.h"
 #include "ray.h"
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+  vec3 ray_origin_to_sphere_center = r.get_origin() - center;
+
+  double a = dot_product(r.get_direction(), r.get_direction());
+  double b = 2.0 * dot_product(ray_origin_to_sphere_center, r.get_direction());
+  double c = dot_product(ray_origin_to_sphere_center, ray_origin_to_sphere_center) - radius * radius;
+
+  double discriminant = b * b - 4 * a * c;
+
+  return discriminant > 0;
+}
+
 color get_ray_color(const ray& r) {
+  if (hit_sphere(point3(0, 0, -1), -.5, r)) {
+    return color(1, 0, 0);
+  }
+
   vec3 unit_direction = get_unit_vector(r.get_direction());
   double t = 0.5 * (unit_direction.y() + 1.0);
 
