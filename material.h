5359a7d532205ddd802870665558a67a89364edd
rviu <134678217+rviu@users.noreply.github.com>
2023-05-27 15:05:05 +0000

    Add fuzzy reflection to metal

--- material.h
+++ material.h
@@ -37,15 +37,16 @@ public:
 class metal : public material {
 private:
   color albedo;
+  double fuzz;
 
 public:
-  metal(const color& a) : albedo(a) {}
+  metal(const color& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
   virtual bool scatter(
       const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
   ) const override {
     vec3 reflected = reflect(get_unit_vector(r_in.get_direction()), rec.normal);
-    scattered = ray(rec.p, reflected);
+    scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
     attenuation = albedo;
 
     return (dot_product(scattered.get_direction(), rec.normal) > 0);
